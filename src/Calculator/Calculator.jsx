6615857d406bec4a35a439c9a67780c27f9105da
nivas <srinivasa@Administrators-MacBook-Pro-8.local>
2019-05-14 08:08:02 +0530

    State chages

--- src/Calculator/Calculator.jsx
+++ src/Calculator/Calculator.jsx
@@ -6,10 +6,27 @@ class Calculator extends React.Component {
     b: 5
   };
 
+  setA = e => {
+    this.setState({
+      a: parseInt(e.target.value)
+    });
+  };
+
+  setB = e => {
+    this.setState({
+      b: parseInt(e.target.value)
+    });
+  };
+
   render() {
     const add = this.state.a + this.state.b;
     return (
       <div>
+        <input className='inputA' onChange={this.setA} />
+        <input onChange={this.setB} />
+        <br />
+        {'A:' + this.state.a + ' B: ' + this.state.b}
+        <br />
         {'Add : ' + add}
         <br />
         {'Sub : ' + (this.state.a - this.state.b)}
